69d66b253ccbe9a7ba38e8b9b140b2d384c8b5c6
webmas0124 <webmaster9124@gmail.com>
2021-06-25 19:48:38 -0400

    warning for unused variable

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,8 +22,7 @@ module.exports = {
     'plugin:prettier/recommended',
   ],
   rules: {
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': ['error'],
+    '@typescript-eslint/no-unused-vars': ['warn'],
     '@typescript-eslint/no-var-requires': 'off',
     'react/prop-types': 'off',
     'react/jsx-uses-react': 'off',
